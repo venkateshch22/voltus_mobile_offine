4be20e2c3e1117910d0cfd91a0f8312a641290b7
venkatesh charla <venkatesh.charla@voltuswave.com>
2024-06-26 22:32:12 +0530

    ADD : static screens for submitted Reports and add Report

--- src/screens/AddReportScreen.js
+++ src/screens/AddReportScreen.js
@@ -1,14 +1,66 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, View, Image} from 'react-native';
+import React from 'react';
+import {IconButton, Text, useTheme, TextInput} from 'react-native-paper';
+import {useSelector} from 'react-redux';
+import WebView from 'react-native-webview';
 
-const AddReportScreen = () => {
+const AddReportScreen = ({navigation}) => {
+  const theme = useTheme();
+  const userData = useSelector(state => state.user.user);
+  console.log(userData);
   return (
-    <View>
-      <Text>AddReportScreen</Text>
+    <View style={{flex: 1, backgroundColor: theme.colors.background}}>
+      <View
+        style={{
+          ...styles.headerContainer,
+          backgroundColor: theme.colors.primaryContainer,
+          elevation: 3,
+        }}>
+        <IconButton
+          icon="arrow-left"
+          size={25}
+          onPress={() => navigation.goBack()}
+        />
+        <Text>Add Report</Text>
+      </View>
+      {/* <View style={{flex: 1}}>
+       
+
+      </View> */}
+      <WebView
+        source={{uri: 'file://android_asset/web/index.html'}}
+        javaScriptEnabled={true}
+        originWhitelist={['*']}
+        allowFileAccess={true}
+        allowUniversalAccessFromFileURLs={true}
+        onError={syntheticEvent => {
+          const {nativeEvent} = syntheticEvent;
+          console.log('native event', nativeEvent);
+        }}
+        injectedJavaScriptObject={{customValue:{
+          
+        }}}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default AddReportScreen
+export default AddReportScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  headerContainer: {
+    height: 50,
+    backgroundColor: '#93b1a7',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  // imageContainer: {
+  //   alignItems: 'center',
+  //   marginTop: 150,
+  //   marginBottom: 10,
+  // },
+  // image: {
+  //   width: 100,
+  //   height: 100,
+  // },
+});
--- src/screens/SubmittedReportsScreen.js
+++ src/screens/SubmittedReportsScreen.js
@@ -1,14 +1,159 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {Pressable, TouchableOpacity, StyleSheet, View} from 'react-native';
+import {useState, useEffect} from 'react';
+import {useAuth} from '../context/AuthContext';
+import {
+  Button,
+  Text,
+  Menu,
+  Card,
+  useTheme,
+  Surface,
+  IconButton,
+  Icon,
+  Portal,
+} from 'react-native-paper';
+import {useDispatch, useSelector} from 'react-redux';
+import NetInfo from '@react-native-community/netinfo';
 
-const SubmittedReportsScreen = () => {
+const SubmittedReportsScreen = ({navigation}) => {
+  const theme = useTheme();
+  const [netorkStatus, setNetworkStatus] = useState();
+
+  const checkNetworkStatus = () => {
+    NetInfo.fetch().then(state => {
+      setNetworkStatus(state.isConnected);
+    });
+  };
+
+  useEffect(() => {
+    const statusIntervalId = setInterval(checkNetworkStatus, 100);
+    return () => {
+      clearInterval(statusIntervalId);
+    };
+  }, []);
+
   return (
-    <View>
-      <Text>SubmittedReportsScreen</Text>
+    <View style={{flex: 1, backgroundColor: theme.colors.background}}>
+      <View
+        style={{
+          ...styles.headerContainer,
+          backgroundColor: theme.colors.primaryContainer,
+          elevation: 3,
+        }}>
+        <IconButton
+          icon="arrow-left"
+          size={25}
+          onPress={() => navigation.goBack()}
+        />
+        <Text style={{flex: 1}}>Submitted Reports</Text>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Icon
+            source="circle"
+            size={10}
+            color={netorkStatus ? '#008000' : '#bf0603'}
+          />
+          <Text variant="labelSmall" style={{paddingLeft: 3}}>
+            {netorkStatus ? 'Online' : 'Offlien'}
+          </Text>
+        </View>
+      </View>
+      <View
+        style={{
+        //   flex: 1,
+          flexDirection: 'row',
+          // gap: 30,
+          justifyContent: 'space-between',
+          marginHorizontal: 25,
+          marginVertical: 20,
+        }}>
+        <TouchableOpacity
+          activeOpacity={0.95}
+          onPress={() => console.log('first')}>
+          <Surface
+            elevation={2}
+            style={{
+              backgroundColor: theme.colors.primary,
+              width: 90,
+              height: 35,
+              borderRadius: 5,
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexDirection: 'row',
+            }}>
+            <Icon source="sync" color={theme.colors.onPrimary} size={17} />
+            <Text
+              variant="bodyMedium"
+              style={{
+                textAlign: 'center',
+                color: theme.colors.onPrimary,
+                paddingLeft: 5,
+              }}>
+              Sync All
+            </Text>
+          </Surface>
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={0.95}
+          onPress={() => navigation.navigate('AddReport')}>
+          <Surface
+            elevation={2}
+            style={{
+              backgroundColor: theme.colors.primary,
+              width: 90,
+              height: 35,
+              borderRadius: 5,
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexDirection: 'row',
+            }}>
+            <Icon source="plus" color={theme.colors.onPrimary} size={17} />
+            <Text
+              variant="bodyMedium"
+              style={{
+                textAlign: 'center',
+                color: theme.colors.onPrimary,
+                paddingLeft: 5,
+              }}>
+              DMR
+            </Text>
+          </Surface>
+        </TouchableOpacity>
+      </View>
+      <View style={{flex: 1, alignItems: 'center',marginTop:'50%'}}>
+        <View
+          style={{
+            width: 150,
+            height: 150,
+            backgroundColor: theme.colors.surfaceVariant,
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 6,
+          }}>
+          <Icon source="text" color={theme.colors.secondary} size={100} />
+        </View>
+        <Text
+          variant="titleMedium"
+          style={{marginTop: 5, color: theme.colors.secondary}}>
+          No Reports
+        </Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default SubmittedReportsScreen
+export default SubmittedReportsScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  headerContainer: {
+    height: 50,
+    backgroundColor: '#93b1a7',
+    flexDirection: 'row',
+    alignItems: 'center',
+    // justifyContent: 'flex-start',
+  },
+});
